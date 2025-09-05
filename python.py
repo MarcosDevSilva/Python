00f2bde4660142179f04a55773c2cf439ae8ae7a
Marcos <tassio50000@gamail.com>
2025-09-04 21:32:16 -0300

    informando so o valor e maior ou meno

--- python.py
+++ python.py
@@ -1,8 +1,13 @@
 numero = 10
 palpite = input("digite um valor: ")
 palpite = int(palpite)
+diferenca = numero - palpite
 
-if numero == palpite:
+
+if diferenca == 0:
     print("vc acertou.")
+elif diferenca > 0:
+    print("Vc erro o numero e maior")
+
 else:
-    print("vc errou.")
+    print("vc errou o numero e maior")
