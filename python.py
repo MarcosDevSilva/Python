9ec5a474c5df43e449958f6c5ccabfd94111e02b
Marcos <tassio50000@gamail.com>
2025-09-11 21:11:09 -0300

    while repetir 5 vezes

--- python.py
+++ python.py
@@ -1,19 +1,23 @@
-numero = 10
-palpite = input("digite um valor (1 a 100): ")
-palpite = int(palpite)
-diferenca = numero - palpite
+contador = 0
+while contador < 5:
+    numero = 10
+    palpite = input("digite um valor (1 a 100): ")
+    palpite = int(palpite)
+    diferenca = numero - palpite
 
 
-if diferenca == 0:
-    print("vc acertou.")
-elif diferenca > 0:
-    if diferenca <= 10:
-        print("Vc erro o numero e maior (esta quente)")
-    else: 
-        print("Vc erro o numero e maior (esta frio)")
+    if diferenca == 0:
+        print("vc acertou.")
+        break
+    elif diferenca > 0:
+        if diferenca <= 10:
+            print("Vc erro o numero e maior (esta quente)")
+        else: 
+            print("Vc erro o numero e maior (esta frio)")
 
-else:
-    if abs(diferenca) <=10:
-        print("vc errou o numero e maior (esta quente)")
-    else: 
-        print("vc errou o numero e maior (esta frio)")
+    else:
+        if abs(diferenca) <=10:
+            print("vc errou o numero e maior (esta quente)")
+        else: 
+            print("vc errou o numero e maior (esta frio)")
+    contador = contador +1
